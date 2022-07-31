a79f86a42f8a047f0cd4667f9afa00e417d9a577
Loïc Baccigalupi <loic.baccigalupi@gmail.com>
2022-07-31 04:20:17 +0200

    converted to vite

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 import { ChakraProvider } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
         <App/>
       </BrowserRouter>
     </ChakraProvider>
+
   </React.StrictMode>
-);
+)
